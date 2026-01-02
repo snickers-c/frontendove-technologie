002c006a405da57bbe74c0cd5ec2a6247d07fbd4
matus <1matus.gabas@gmail.com>
2026-01-02 23:56:03 +0100

    Starter navigation with working dropdowns

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import Navigation from './components/Navigation.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Navigation></Navigation>
+  <!-- <RouterView></RouterView> -->
 </template>
 
 <style scoped></style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,73 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Navigation',
+  data() {
+    return {
+      links: ["Oblečenie", "Obuv", "Závažia", "Konštrukcie", "Stroje", "Pomôcky"],
+      dropdown: false,
+      count: 1
+    }
+  },
+  methods: {
+    hovered(event: any) {
+      event.currentTarget.children[1].classList.remove("hide");
+      this.dropdown = true;
+    },
+    hide(event: any) {
+      event.currentTarget.children[1].classList.add("hide");
+      this.dropdown = false;
+    }
+  },
+  mounted() { },
+})
+</script>
+
+<template>
+  <div class="nav">
+    <div class="nav-main">
+      <div class="nav-left">
+        <div class="nav-logo">
+          <img src="/images/Logo.svg" alt="logo">
+        </div>
+
+        <div class="nav-search">
+          <img src="/images/Search.svg" alt="-O">
+          <input type="text" placeholder="Vyhladať">
+        </div>
+      </div>
+
+      <div class="nav-icons">
+        <img src="/images/Basket.svg" alt="košík">
+        <img src="/images/Favorite.svg" alt="oblľúbené">
+        <img id="profile-icon" src="/images/User-icon.png" alt="profil">
+      </div>
+    </div>
+
+    <div class="nav-links">
+      <div @mouseover="hovered" @mouseleave="hide" v-for="link in links">
+        <RouterLink to="">{{ link }}</RouterLink>
+        <div class="nav-dropdown hide">
+          <div class="list">
+            <p>Tričká</p>
+            <ul>
+              <li>a</li>
+              <li>b</li>
+              <li>c</li>
+            </ul>
+          </div>
+
+          <div class="list">
+            <p>Tepláky</p>
+            <ul>
+              <li>d</li>
+              <li>e</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
